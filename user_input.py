601caee5b95cfe4ecb12f2ee5fbe0335c65c113e
HumarFaaruq <139682128+HumarFaaruq@users.noreply.github.com>
2024-04-12 10:12:52 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+Name = input("Tell me your name: ")
+Age = input("Tell me your age: ")
+Location =input("Tell me your location: ")
+
+print("Name")
+print (Hello "Faruq",i am "24 years old", and lived in "Agbara")
